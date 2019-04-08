edfadec31d206632bbdf52e8ebf9e359297af036
disclearing <realhcfus@gmail.com>
2019-04-08 18:03:29 +0100

    re coviring php and html beacuse why not

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+<title> me relearning html and php</title/
+</head>
+<body>
+
+
+<?php
+
+        $name = "Disclearing";
+
+        echo "Nigger, " . $name
+
+
+?>
+
+</body>
+</html>
